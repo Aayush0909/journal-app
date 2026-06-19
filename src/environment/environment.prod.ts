36c981ccccba4f9cb3467e643805582675c23b42
Aayush Chhalerya <aayushknight@gmail.com>
2026-06-20 00:43:59 +0530

    production environment file

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'journal-app-backend-production-1b89.up.railway.app'  // fill this in Phase 12
+  apiUrl: 'journal-app-backend-production-1b89.up.railway.app'  
 };
