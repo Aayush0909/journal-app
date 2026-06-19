bbbadc2c5902f2b22e62d77e9b8e7a3e2f238a80
Aayush Chhalerya <aayushknight@gmail.com>
2026-06-20 01:04:35 +0530

    fix railway api url

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'journal-app-backend-production-1b89.up.railway.app'  
+  apiUrl: 'https://journal-app-backend-production-1b89.up.railway.app'  
 };
